c0e95800120c6656fa11d466a73e25be86d22d63
jacattrongnlh <jacattrongnlh@volio.vn>
2023-02-02 09:32:12 +0700

    init repo

---
+++ ButtonAudio.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAudio : MonoBehaviour
+{
+    [SerializeField] AudioClip buttonSound;
+
+    private void Start()
+    {
+        var button = GetComponent<Button>();
+        if (button != null) { button.onClick.AddListener(Play); }
+    }
+
+    public void Play()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayButtonTapSfx();
+        }
+    }
+}
